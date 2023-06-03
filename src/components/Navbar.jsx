ac4aa95f67d7c61a9bb12945ed9c7b9d957765a6
Asfand Yar <asfandyar@novoassociates.com>
2023-06-03 21:55:36 +0500

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from "../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="py-8 w-full max-w-7xl mx-auto flex items-center justify-between">
+        <img className="w-[150px]" src={Logo} alt="Huzefa Engineering"/>
+        <ul className="uppercase text-lg flex gap-x-24">
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
