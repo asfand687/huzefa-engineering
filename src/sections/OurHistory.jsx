2c1fd1cad1569e4ca3d10b28cc4e82901f970de0
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:35:05 +0500

    added media queries

--- src/sections/OurHistory.jsx
+++ src/sections/OurHistory.jsx
@@ -5,7 +5,7 @@ import OurServices from './OurServices'
 const OurHistory = () => {
   return (
     <section className="bg-[#3B89ED] text-white flex gap-x-6">
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full lg:max-w-6xl max-[1300px]:max-w-7xl mx-auto">
         <HistorySectionTop/>
         <OurServices/>
       </div>
