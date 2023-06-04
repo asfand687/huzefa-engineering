46765b2f05689bdceeef2008097137136820a9a0
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:34:20 +0500

    fixed layout for narrow screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.svg"
 const Navbar = () => {
   return (
     <nav>
-      <div className="py-8 w-full max-w-7xl mx-auto flex items-center justify-between">
+      <div className="py-8 w-full lg:max-w-6xl max-[1300px]:max-w-7xl mx-auto flex items-center justify-between">
         <img className="w-[150px]" src={Logo} alt="Huzefa Engineering"/>
         <ul className="uppercase text-lg flex gap-x-24">
           <li>About Us</li>
