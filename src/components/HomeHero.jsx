3acb6e2a49606f3b48bef7f494724f98336de17f
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:34:10 +0500

    fixed spacing for narrow screens

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -4,9 +4,9 @@ import Hero from '../assets/hero-image.png'
 const HomeHero = () => {
   return (
     <section>
-      <div className="w-full max-w-7xl mx-auto flex gap-x-6 pt-14">
-        <article className="w-full max-w-[600px] flex justify-end items-center">
-          <h2 className="text-8xl font-semibold text-black">
+      <div className="w-full lg:max-w-6xl max-[1300px]:max-w-7xl mx-auto flex gap-x-6 pt-14">
+        <article className="w-full lg:max-w-[550px] max-[1300px]:max-w-[600px] flex justify-end items-center">
+          <h2 className="lg:text-7xl max-[1300px]:text-8xl font-semibold text-black">
             Leaders In <br/> 
             <span className="text-[#0065E7]">
               Business <br/> Growth
@@ -14,8 +14,8 @@ const HomeHero = () => {
           </h2>
         </article>
         <article className="flex-1 flex items-center gap-x-8">
-          <img className="w-[350px] h-700px object-contain" src={Hero} alt="Hero Image" />
-          <p className="text-2xl max-w-[250px]">
+          <img className="w-full max-w-[350px] lg:h-[600px] max-[1300px]:h-700px object-cover" src={Hero} alt="Hero Image" />
+          <p className="lg:text-xl max-[1300px]:text-2xl max-w-[250px]">
             In today's business, customer satisfaction is of utmost importance. Keeping this fact in mind, we at Huzefa Engineering Services (HES) assist our customers in any way we can.
           </p>
         </article>
