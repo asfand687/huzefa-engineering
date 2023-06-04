bccbe1e9bc99eef0fc8cb5d9351160266ec6db04
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:01:45 +0500

    added new sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HomeHero from './components/HomeHero'
 import LoremSection from './sections/LoremSection'
+import OurClientsSection from './sections/OurClientsSection'
+import OurHistory from './sections/OurHistory'
+import SpecialitySection from './sections/SpecialitySection'
+import ContactUs from './sections/ContactUs'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +17,11 @@ function App() {
       <Navbar/>
       <HomeHero/>
       <LoremSection/>
+      <OurClientsSection/>
+      <OurHistory/>
+      <SpecialitySection/>
+      <ContactUs/>
+      <Footer/>
     </section>
   )
 }
