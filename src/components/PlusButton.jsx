b09e9ab6b2aac40c27027a166702b9883b5dea05
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:03:03 +0500

    added plus button component

---
+++ src/components/PlusButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlusButton = () => {
+  return (
+    <div className="w-10 h-10 text-4xl font-semibold rounded-md flex justify-center items-center border border-white">
+      +
+    </div>
+  )
+}
+
+export default PlusButton
