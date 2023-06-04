1c80cd76044b5933c10ff103a04123dd503d4a9a
Asfand Yar <asfandyar@novoassociates.com>
2023-06-04 13:03:59 +0500

    added our services

---
+++ src/sections/OurServices.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import SectionTitle from '../components/SectionTitle'
+import ServiceBlock from '../components/ServiceBlock'
+
+const servicesData = [
+  {
+    id: "01",
+    title: "Fabrication",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: "02",
+    title: "Maintenance",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: "03",
+    title: "Fabrication",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: "04",
+    title: "Maintenance",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  }
+]
+const OurServices = () => {
+  return (
+    <section className="pt-10">
+      <SectionTitle
+        title="Our Services"
+        color="#ffffff"
+      />
+      <div className="grid grid-cols-2 gap-10">
+        {
+          servicesData.map(service => (
+            <ServiceBlock
+              key={service.id}
+              id={service.id}
+              title={service.title}
+              description={service.description}
+            />
+          ))
+        }
+      </div>
+    </section>
+  )
+}
+
+export default OurServices
