5f2fc594a09ea5085d47f34bb831585de18ad9dd
Asfand Yar <asfandyar687@gmail.com>
2023-06-21 17:33:52 +0500

    made logo grid responsive

--- src/components/LogoGrid.jsx
+++ src/components/LogoGrid.jsx
@@ -20,25 +20,25 @@ import HSM from "../assets/hsm.png"
 
 const LogoGrid = () => {
   return (
-    <section className="grid grid-cols-6 gap-8">
-      <img className="w-[150px] h-[150px] object-contain" src={PandG} alt="P and G"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Shan} alt="Shan"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Tapal} alt="Tapal"/>
-      <img className="w-[150px] h-[150px] object-contain" src={MTF} alt="MTF"/>
-      <img className="w-[150px] h-[150px] object-contain" src={AmreliSteels} alt="Amreli Steels"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Cadbury} alt="Cadbury"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Siraj} alt="Siraj"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Hino} alt="Hino"/>
-      <img className="w-[150px] h-[150px] object-contain" src={National} alt="National"/>
-      <img className="w-[150px] h-[150px] object-contain" src={AES} alt="AES"/>
-      <img className="w-[150px] h-[150px] object-contain" src={PakistanCables} alt="PakistanCables"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Avery} alt="Avery"/>
-      <img className="w-[150px] h-[150px] object-contain" src={FFC} alt="FFC"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Exide} alt="Exide"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Lakson} alt="Lakson"/>
-      <img className="w-[150px] h-[150px] object-contain" src={NationalPower} alt="National Power"/>
-      <img className="w-[150px] h-[150px] object-contain" src={Dupont} alt="Dupont"/>
-      <img className="w-[150px] h-[150px] object-contain" src={HSM} alt="HSM"/>
+    <section className="grid grid-cols-3 gap-4 md:grid-cols-6 md:gap-8">
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={PandG} alt="P and G"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Shan} alt="Shan"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Tapal} alt="Tapal"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={MTF} alt="MTF"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={AmreliSteels} alt="Amreli Steels"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Cadbury} alt="Cadbury"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Siraj} alt="Siraj"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Hino} alt="Hino"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={National} alt="National"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={AES} alt="AES"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={PakistanCables} alt="PakistanCables"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Avery} alt="Avery"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={FFC} alt="FFC"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Exide} alt="Exide"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Lakson} alt="Lakson"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={NationalPower} alt="National Power"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={Dupont} alt="Dupont"/>
+      <img className="w-[80px] md:w-[150px] h-[83px] md:h-[150px] mx-auto object-contain" src={HSM} alt="HSM"/>
 
     </section>
   )
