b58af9b35795da9dda534e8c2b9a28df514a0184
Asfand Yar <asfandyar687@gmail.com>
2023-06-21 17:34:13 +0500

    made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,9 @@ import Logo from "../assets/logo.svg"
 const Navbar = () => {
   return (
     <nav>
-      <div className="py-8 w-full lg:max-w-6xl max-[1300px]:max-w-7xl mx-auto flex items-center justify-between">
+      <div className="py-8 w-[clamp(300px,80vw,1300px)] mx-auto flex items-center justify-between">
         <img className="w-[150px]" src={Logo} alt="Huzefa Engineering"/>
-        <ul className="uppercase text-lg flex gap-x-24">
+        <ul className="hidden uppercase text-lg lg:flex gap-x-24">
           <li>About Us</li>
           <li>Services</li>
           <li>Contact Us</li>
