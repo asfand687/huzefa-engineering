2c558dcf6bc3d9286aa9cec435ba0db6e1fc3076
Asfand Yar <asfandyar687@gmail.com>
2023-06-21 17:34:40 +0500

    made CEO message responsive

--- src/sections/CEOMessage.jsx
+++ src/sections/CEOMessage.jsx
@@ -3,10 +3,10 @@ import CEO from '../assets/ceo.png'
 
 const CEOMessage = () => {
   return (
-    <section className="flex gap-x-6 pt-10">
+    <section className="flex flex-col md:flex-row gap-6 pt-10">
       <article>
         <div className="flex flex-col gap-y-1 pb-3">
-          <h2 className="text-[clamp(4rem,8vw,150px)] font-extrabold leading-[120px]">CEO</h2>
+          <h2 className="text-[clamp(8rem,8vw,150px)] font-extrabold leading-[120px]">CEO</h2>
           <h3 className="text-5xl font-extrabold">Message</h3>
         </div>
         <p className="w-full max-w-[500px] lg:text-2xl max-[1300px]:text-3xl font-semibold">
@@ -14,7 +14,7 @@ const CEOMessage = () => {
         </p>
       </article>
       <article>
-        <img className="w-[630px] h-[820px] object-cover" src={CEO} alt="CEO"/>
+        <img className="w-full md:w-[630px] md:h-[820px] object-cover" src={CEO} alt="CEO"/>
       </article>
     </section>
   )
