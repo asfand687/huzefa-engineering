42dcdf6f278ed11e800a0370c7c2dbb4f1b5777e
Asfand Yar <asfandyar687@gmail.com>
2023-06-21 17:35:00 +0500

    made history section responsive

--- src/sections/HistorySectionTop.jsx
+++ src/sections/HistorySectionTop.jsx
@@ -3,13 +3,13 @@ import Buildings from '../assets/buildings.png'
 
 const HistorySectionTop = () => {
   return (
-    <div className="flex items-center gap-x-6">
+    <div className="flex flex-col md:flex-row items-center gap-6">
       <article className="w-full max-w-[500px]">
-        <h2 className="text-6xl pb-3">Our History</h2>
-        <p className="text-2xl">
+        <h2 className="text-4xl font-bold md:font-normal md:text-6xl pb-3">Our History</h2>
+        <p className="text-base font-bold md:font-normalmd:text-2xl">
           HUZEFA ENGINEERING SERVICES (HES) is committed, as a company, to conduct its business in a responsible manner that protects public interest and provides a safe atmosphere for continuous business growth and development. By fulfilling this responsibility, HES enhances the ability to provide competitive and profitable services in the long term.
         </p>
-        <div className="pt-12">
+        <div className="pt-5 md:pt-12">
           <button 
             type="button"
             className="text-2xl py-2 px-7 rounded-lg border border-white"
