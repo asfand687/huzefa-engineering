965d9ed60ba063c9776ef5ffb792287d023abfc8
Asfand Yar <asfandyar687@gmail.com>
2023-06-23 17:26:59 +0500

    changed content

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -5,7 +5,7 @@ const HomeHero = () => {
   return (
     <section>
       <div className="w-[clamp(270px,80vw,1300px)] mx-auto flex flex-col lg:flex-row gap-x-6 pt-14 gap-y-7">
-        <article className="w-full lg:max-w-[550px] max-[1300px]:max-w-[600px] flex md:justify-end items-center">
+        <article className="flex items-center">
           <h2 className="text-[clamp(3.5rem,6vw,6rem)] leading-[clamp(3.5rem,6vw,6rem)] font-semibold text-black">
             Leaders In <br/> 
             <span className="text-[#0065E7]">
@@ -15,8 +15,8 @@ const HomeHero = () => {
         </article>
         <article className="flex-1 flex flex-col lg:flex-row items-center gap-x-8 gap-y-7 pb-12 md:pb-0">
           <img className="w-full max-w-[350px] lg:h-[600px] max-[1300px]:h-700px object-cover" src={Hero} alt="Hero Image" />
-          <p className="lg:text-xl max-[1300px]:text-2xl lg:max-w-[250px]">
-            In today's business, customer satisfaction is of utmost importance. Keeping this fact in mind, we at Huzefa Engineering Services (HES) assist our customers in any way we can.
+          <p className="lg:text-xl max-[1300px]:text-2xl lg:max-w-[450px]">
+            Welcome to Huzefa Engineering Services Established in 1995, Huzefa Engineering Services is a trusted name in the engineering industry. With over 25 years of experience, we have earned a reputation for excellence and reliability. We provide a wide range of engineering solutions to multinational, semi-government, and private organizations, catering to their mechanical and industrial fabrication, erection, supplies, and maintenance needs. At Huzefa Engineering Services, our commitment to customer satisfaction is paramount, and we continuously strive to exceed expectations.
           </p>
         </article>
       </div>
