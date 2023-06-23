a6b92f30cd3752eb50062215d7b05717e010be9b
Asfand Yar <asfandyar687@gmail.com>
2023-06-23 17:27:27 +0500

    updated content

--- src/sections/HistorySectionTop.jsx
+++ src/sections/HistorySectionTop.jsx
@@ -6,8 +6,8 @@ const HistorySectionTop = () => {
     <div className="flex flex-col md:flex-row items-center gap-6">
       <article className="w-full max-w-[500px]">
         <h2 className="text-4xl font-bold md:font-normal md:text-6xl pb-3">Our History</h2>
-        <p className="text-base font-bold md:font-normalmd:text-2xl">
-          HUZEFA ENGINEERING SERVICES (HES) is committed, as a company, to conduct its business in a responsible manner that protects public interest and provides a safe atmosphere for continuous business growth and development. By fulfilling this responsibility, HES enhances the ability to provide competitive and profitable services in the long term.
+        <p className="text-base font-bold md:font-normal md:text-2xl text-justify">
+          Huzefa Engineering Services is a leading provider of comprehensive engineering solutions. We take pride in our vast experience and technical expertise, which enable us to deliver exceptional results on every project. With a strong focus on quality, precision, and customer satisfaction, we have built long-term relationships with esteemed clients across various industries. Our core values of professionalism, reliability, and innovation drive us to constantly improve and deliver cutting-edge solutions.
         </p>
         <div className="pt-5 md:pt-12">
           <button 
