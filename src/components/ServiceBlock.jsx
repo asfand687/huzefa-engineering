81f7bcd501283c7b4edf6586d314b575ea68ef62
Asfand Yar <asfandyar687@gmail.com>
2023-06-23 17:27:07 +0500

    changed font size

--- src/components/ServiceBlock.jsx
+++ src/components/ServiceBlock.jsx
@@ -7,7 +7,7 @@ const ServiceBlock = ({id, title, description}) => {
       <h3 className="text-2xl font-extrabold">
         {id}
       </h3>
-      <h2 className="text-[clamp(1.5rem,4vw,3rem)] font-bold pt-5 pb-3">
+      <h2 className="text-[clamp(1.3rem,3.5vw,2.5rem)] font-bold pt-5 pb-3">
         {title}
       </h2>
       <p className="text-black text-base md:text-lg font-semibold w-full max-w-[500px] pb-4">
