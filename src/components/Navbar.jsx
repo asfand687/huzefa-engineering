bc85d1099058821c60897b45a02267509b34712c
Asfand Yar <asfandyar687@gmail.com>
2023-06-24 22:07:19 +0500

    added links to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Logo from "../assets/logo.svg"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
       <div className="py-8 w-[clamp(300px,80vw,1300px)] mx-auto flex items-center justify-between">
-        <img className="w-[150px]" src={Logo} alt="Huzefa Engineering"/>
+        <Link to="/">
+          <img className="w-[150px]" src={Logo} alt="Huzefa Engineering"/>
+        </Link>
         <ul className="hidden uppercase text-lg lg:flex gap-x-24">
-          <li>About Us</li>
+          <li><Link to={"/about"}>About Us</Link></li>
           <li>Services</li>
           <li>Contact Us</li>
         </ul>
