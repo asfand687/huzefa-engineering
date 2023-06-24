2b18048579fbc0f0a1c7faf43b9a471100b82f25
Asfand Yar <asfandyar687@gmail.com>
2023-06-24 20:04:43 +0500

    created section index file

---
+++ src/sections/index.js
@@ -0,0 +1,24 @@
+import HomeHero from "./HomepageSections/HomeHero";
+import CEOMessage from "./HomepageSections/CEOMessage";
+import ContactUs from "./HomepageSections/ContactUs";
+import OurHistory from "./HomepageSections/OurHistory";
+import HistorySectionTop from "./HomepageSections/HistorySectionTop";
+import LoremSection from "./HomepageSections/LoremSection";
+import OurClientsSection from "./HomepageSections/OurClientsSection";
+import OurServices from "./HomepageSections/OurServices";
+import SpecialitySection from "./HomepageSections/SpecialitySection";
+
+
+
+export default {
+  HomepageHeroSection: HomeHero,
+  HomepageCEOMessage: CEOMessage,
+  HomepageContactUs: ContactUs,
+  HomepageOurHistorySection: OurHistory,
+  HomepageOurHistorySectionTop: HistorySectionTop,
+  HomepageLoremSection: LoremSection,
+  HomepageClientsSection: OurClientsSection,
+  HomepageServicesSection: OurServices,
+  HomepageSpecialitySection: SpecialitySection
+
+}
