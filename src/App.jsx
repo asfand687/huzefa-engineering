a781d016081ca865ff2f5251d8199e8a0a322da5
Asfand Yar <asfandyar687@gmail.com>
2023-06-24 20:03:46 +0500

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import OurHistory from './sections/OurHistory'
 import SpecialitySection from './sections/SpecialitySection'
 import ContactUs from './sections/ContactUs'
 import Footer from './components/Footer'
+import {
+  Homepage
+} from "./pages"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,11 +17,7 @@ function App() {
   return (
     <section className='overflow-clip'>
       <Navbar/>
-      <HomeHero/>
-      <OurClientsSection/>
-      <OurHistory/>
-      <SpecialitySection/>
-      <ContactUs/>
+      <Homepage/>
       <Footer/>
     </section>
   )
