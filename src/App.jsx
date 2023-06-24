560d7247ff1ec7ce46f65309d63a4c2b199fc3d9
Asfand Yar <asfandyar687@gmail.com>
2023-06-24 22:07:02 +0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
-import HomeHero from './components/HomeHero'
-import OurClientsSection from './sections/OurClientsSection'
-import OurHistory from './sections/OurHistory'
-import SpecialitySection from './sections/SpecialitySection'
-import ContactUs from './sections/ContactUs'
 import Footer from './components/Footer'
 import {
-  Homepage
+  Homepage,
+  About
 } from "./pages"
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <section className='overflow-clip'>
-      <Navbar/>
-      <Homepage/>
-      <Footer/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="about/" element={<About />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </section>
   )
 }
