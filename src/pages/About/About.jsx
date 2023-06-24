79c80d5ef293879263418cb42fe16c9d27d98a40
Asfand Yar <asfandyar687@gmail.com>
2023-06-24 22:07:54 +0500

    added hero section to about

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AboutHero } from '../../sections'
+
+const About = () => {
+  return (
+    <div>
+      <AboutHero/>
+    </div>
+  )
+}
+
+export default About
