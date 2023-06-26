83c3c9e07129eeeb2008b117b5afc4b69b3c1e43
Asfand Yar <asfandyar687@gmail.com>
2023-06-26 17:00:53 +0500

    added about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { AboutHero } from '../../sections'
+import { AboutAdditionalServices, AboutDescription, AboutHero, ContactUs } from '../../sections'
 
 const About = () => {
   return (
     <div>
       <AboutHero/>
+      <AboutDescription/>
+      <AboutAdditionalServices/>
+      <ContactUs/>
     </div>
   )
 }
