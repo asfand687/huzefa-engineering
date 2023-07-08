0b62ad1e553ce6a80f7414b50bfe681d4dedfb56
Asfand Yar <asfandyar687@gmail.com>
2023-07-08 13:47:47 +0500

    COMPONENT: fixed the widths of links for mobile

--- src/sections/Services/ServicesInfoBlock.jsx
+++ src/sections/Services/ServicesInfoBlock.jsx
@@ -4,7 +4,8 @@ const ServicesInfoBlock = ({title, headingColor = "", description, subService, n
   return (
     <div className="w-full flex flex-col md:flex-row gap-8">
       <article className="flex-1 flex md:justify-end font-bold text-3xl md:text-4xl">
-        <div className={`${headingColor}`}>
+        <div className="hidden md:block flex-1"/>
+        <div className={`flex-1 ${headingColor}`}>
           {subService && <h4>{number}</h4>}
           <h2 className={`max-w-[260px] md:max-w-xs `}>{title}</h2>
         </div>
