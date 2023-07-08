db06771277945afb3ce9785d213b48363db439a4
Asfand Yar <asfandyar687@gmail.com>
2023-07-08 13:30:08 +0500

    CHORE: added metal fabrication service page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,10 @@ import About from "./About/About"
 import GeneralOrderSupplies from "./Services/GeneralOrderSupplies";
 import Services from "./Services/Services"
 import BulkMovementSolutions from "./Services/BulkMovementSolutions";
+import MetalFabricationServices from "./Services/MetalFabricationServices";
 
 export {
+  MetalFabricationServices,
   Homepage,
   About,
   GeneralOrderSupplies,
