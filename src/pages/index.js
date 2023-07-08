99eab275ddcf5db364dac82f8277eb04629bdfa8
Asfand Yar <asfandyar687@gmail.com>
2023-07-08 13:26:00 +0500

    added new pages to exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import Homepage from "./Homepage/Homepage";
 import About from "./About/About"
+import GeneralOrderSupplies from "./Services/GeneralOrderSupplies";
+import Services from "./Services/Services"
+import BulkMovementSolutions from "./Services/BulkMovementSolutions";
 
 export {
   Homepage,
-  About
+  About,
+  GeneralOrderSupplies,
+  Services,
+  BulkMovementSolutions
 }
