6706aa886e6aa7ef07b0f70aea8a2096dbc692ec
Asfand Yar <asfandyar687@gmail.com>
2023-07-08 15:40:55 +0500

    RESPONSIVENESS: changed content sizing

--- src/components/AboutHeadingText.jsx
+++ src/components/AboutHeadingText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutHeadingText = ({text, textColor = "text-[#1E1E1E]"}) => {
   return (
-    <div className="flex justify-center max-w-xl mx-auto">
+    <div className="flex justify-center w-[clamp(280px,40vw,576px)] mx-auto">
       <h2 className={`text-4xl md:text-7xl md:leading-tight font-bold ${textColor}`}>{text}</h2>
     </div>
   )
