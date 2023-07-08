140f88cf6896f6293608fe76b23f6c2f59b32116
Asfand Yar <asfandyar687@gmail.com>
2023-07-08 13:26:30 +0500

    added services page

---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { 
+  ServicesHero, 
+  ContactUs, 
+  ServicesDescription, 
+  ServicesInfoBlock, 
+  AboutCarousel, 
+  ServicesNav
+} from '../../sections'
+
+const About = () => {
+  return (
+    <div>
+      <ServicesHero
+        titleTop="Workshop Fabrication"
+        titleBottom="Services"
+      />
+      <ServicesDescription>
+        <ServicesInfoBlock
+          title="Workshop Fabrication Services"
+          description="At Huzefa Engineering Services, we offer a wide range of workshop fabrication services, providing solutions for various industries. Our expertise is divided into four major categories:"
+        />
+        <AboutCarousel/>
+      </ServicesDescription>
+      <ServicesNav/>
+      <ContactUs/>
+    </div>
+  )
+}
+
+export default About
